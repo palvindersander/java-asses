9ee611721caa047910778e87c45518995d784cfe
Palvinder Sander <palvinderssander@gmail.com>
2019-11-25 14:15:38 +0000

    ass4

--- ass4/intellij/src/com/ass4/Converter.java
+++ ass4/intellij/src/com/ass4/Converter.java
@@ -2,13 +2,20 @@
 
 package com.ass4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+
+enum ConvertMode {BIN2HEX, HEX2BIN, BIN2DEC, DEC2BIN}
 
 public class Converter {
 
-    public static enum ConvertMode {BIN2HEX, HEX2BIN, BIN2DEC, DEC2BIN}
-
     private ConvertMode mode;
+
+    private ArrayList<String> inputs;
+
+    private ArrayList<String> outputs;
 
     public Converter(ConvertMode mode) {
         this.mode = mode;
@@ -35,12 +42,32 @@ public class Converter {
         return output;
     }
 
-    public void fromFile​(String filename) {
-
+    public void fromFile(String filename) {
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(filename));
+            ArrayList<String> inputs = new ArrayList<>();
+            String line = "";
+            while ((line = in.readLine()) != null) {
+                inputs.add(line);
+            }
+            in.close();
+            this.inputs = inputs;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> outputs = new ArrayList<>();
+        for (String value : inputs) {
+            outputs.add(this.convert(value));
+        }
+        this.outputs = outputs;
     }
 
-    public ArrayList<String> getInputValues(){return null;}
+    public ArrayList<String> getInputValues() {
+        return inputs;
+    }
 
-    public ArrayList<String> getOutputValues(){return null;}
+    public ArrayList<String> getOutputValues() {
+        return outputs;
+    }
 
 }
