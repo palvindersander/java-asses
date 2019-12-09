288660d520f1f1d4b6ff6cd88c2564dba0f8312d
Palvinder Sander <palvinderssander@gmail.com>
2019-12-09 21:57:52 +0000

    done

--- ass5/assignment5/src/sample/Main.java
+++ ass5/assignment5/src/sample/Main.java
@@ -1,39 +1,75 @@
 package sample;
+//package com.bham.pij.assignments.edgedetector;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.File;
 
 public class Main extends Application {
 
+    private ImageView imgView;
+    private File file;
+
     @Override
-    public void start(Stage stage) throws Exception{
+    public void start(Stage stage) throws Exception {
         mainStage(stage);
         stage.show();
     }
 
-    private void mainStage(Stage stage){
-        stage.setTitle("edge filters");
+    private void mainStage(Stage stage) {
+        stage.setTitle("Image filters");
         VBox root = new VBox();
+        //
+        //
         Menu filesMenu = new Menu("File");
-        MenuItem fileItem1 = new MenuItem("Open");
-        MenuItem fileItem2 = new MenuItem("Close");
+        MenuItem openItem = new MenuItem("Open");
+        MenuItem closeItem = new MenuItem("Close");
         Menu toolsMenu = new Menu("Tools");
-        MenuItem toolsItem1 = new MenuItem("Edge Detection");
-        MenuItem toolsItem2 = new MenuItem("Revert");
-        filesMenu.getItems().addAll(fileItem1, fileItem2);
-        toolsMenu.getItems().addAll(toolsItem1, toolsItem2);
+        MenuItem filterItem = new MenuItem("Edge Detection");
+        MenuItem revertItem = new MenuItem("Revert");
+        filesMenu.getItems().addAll(openItem, closeItem);
+        toolsMenu.getItems().addAll(filterItem, revertItem);
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().add(filesMenu);
         menuBar.getMenus().add(toolsMenu);
         root.getChildren().add(menuBar);
+        //
+        //
         stage.setScene(new Scene(root, 500, 500));
+        //
+        //
+        openItem.setOnAction(actionEvent -> {
+            FileChooser fileOpener = new FileChooser();
+            fileOpener.setTitle("Open a file");
+            file = fileOpener.showOpenDialog(stage);
+            if (file != null) {
+                removeImage(root);
+                loadFileImage(root);
+            }
+        });
     }
 
+    private void loadFileImage(VBox root) {
+        Image img = new Image("file:" + file.getPath());
+        imgView = new ImageView(img);
+        root.getChildren().add(imgView);
+    }
+
+    private void removeImage(VBox root) {
+        root.getChildren().remove(imgView);
+    }
 
     public static void main(String[] args) {
         launch(args);
